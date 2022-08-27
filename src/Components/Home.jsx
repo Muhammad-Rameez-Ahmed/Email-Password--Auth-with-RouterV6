a3999a9acb32ffbd7a8de15f68b94325404565cb
Rameez <muhammadrameezahmed73@gmail.com>
2022-08-27 23:27:04 +0500

    Todo Screen add with forestore

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { db } from "../firbase"
 
 import Navbar from './Navbar'
 
 const Home = () => {
+  const [input, setInput] = useState();
+  
+  const getvalue = (event) => {
+    setInput(event.target.value);
+  };
+  const addvalue = async () => {
+    db.collection("Todo")
+      .doc()
+      .set({
+        value: input,
+      })
+      .then(function () {
+        alert("Value successfully written!");
+        setInput("");
+      })
+      .catch(function (error) {
+        console.error("Error writing Value: ", error);
+      });
+  }
   return (
     <>   
     <Navbar/>
-     <h1>hey home</h1>
-    
+
+    <div  className='center container'>
+     <h1> 📝 Todo</h1>
+     <div className="input-field">
+      <input placeholder=" ✏️ Add todo ?" value={input}    onChange={getvalue} type='email'/><br></br>
+    </div>
+ 
+    <button type='submit' className='btn blue' onClick={addvalue}>Add Todo</button>
+    </div>
      </>
 
   )
 }
 
-export default Home
+export default Home;
